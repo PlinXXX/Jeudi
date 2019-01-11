5fffb96019c6ba620ba1b9e28e4da14f1fb390ee
PlinXXX <46311066+PlinXXX@users.noreply.github.com>
2019-01-11 08:15:20 +0300

    "Fin"

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -22,23 +22,28 @@ end
 
 def lancer 
 	de = rand(1..6)
-	puts de
+	puts "::#{de}"
+	return de
 end 
 
-def action	
-	depl = 10	
-	while depl > 0 
-	puts "Lancer le dé ? (Tap any key) (Q) Quitter"
-	print "> " 
-	rep = gets.chomp.upcase
-		de = lancer
-		break if rep == 'Q'
-		puts de
-		if de == 5 || de == 6
+def action
+	partie = 0
+	win = 0
+	r = "Y"
+	while r != 'Q'
+
+		depl = 10
+		while depl > 0 
+			puts " (Tap any key)Lancer le dé (Q) Quitter"
+			print "> " 
+			rep = gets.chomp.upcase
+			de = lancer
+			break if rep == 'Q'
+			if de == 5 || de == 6
 			depl -= 1 
 			puts full_pyramide(depl) 
 			puts "Vous avez avancé d'une marche"  
-		elsif de == 1 
+			elsif de == 1 
 			if depl < 10 	
 				depl+=1 
 				puts full_pyramide(depl) 
@@ -46,16 +51,24 @@ def action
 			else
 		 		full_pyramide(depl) 
 			end 
-		elsif de == 2 || de == 3 || de == 4	 	
-			full_pyramide(depl) 	
-			puts "Vous n'avez pas bougé"
+			elsif de == 2 || de == 3 || de == 4	 	
+				full_pyramide(depl) 	
+				puts "Vous n'avez pas bougé"
+			end
+			puts "Vous vous trouvez sur la position #{10 - depl}"
+			partie += 1
 		end
-		puts "Vous vous trouvez sur la position #{10 - depl}"
-	end
-	if depl == 0	
+		if depl == 0
+		win+=1
+		puts "Vous avez lancer le de #{partie} fois et avez remporte #{win} victoire(s)"	
 		puts "-------------------"	
 		puts " Vous-avez gagné!!"	
 		puts "-------------------"
+		end
+
+		puts " (Any key)Rejouer \n (Q)Quitter"
+		print "> "
+		r = gets.chomp.upcase
 	end
 end
 
@@ -63,11 +76,14 @@ puts " 1. Afficher une pyramide \n 2. Lancer un dé \n 3. Jouer"
 print "> "
 choix = gets.chomp.to_i
 if choix == 1
-	full_pyramide
+	default = ''
+	depl = default
+	full_pyramide(depl)
 elsif choix == 2
 	lancer
 elsif choix == 3
 	action
 else
 	puts "Veuillez choisir l'un de ces chiffres!"	
-endn
+end
+	
